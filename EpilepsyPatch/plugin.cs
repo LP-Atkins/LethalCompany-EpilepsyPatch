97d62741e8a40714c97feb5235c1c85226f1d667
Paul Atkins <Paul.Atkins@outlook.com.au>
2024-01-22 19:14:52 +1000

    had to fix readme.md

--- EpilepsyPatch/plugin.cs
+++ EpilepsyPatch/plugin.cs
@@ -21,7 +21,7 @@ namespace EpilepsyPatch
     {
         private const string modGUID = "LongParsnip.EpilepsyPatch";
         private const string modName = "EpilepsyPatch";
-        private const string modVersion = "1.0.10.0";
+        private const string modVersion = "1.0.11.0";
         public const bool LogDebugMessages = false;                     //This is for helping with developing the transpiler code, to find the correct IL to modify.
 
         private readonly Harmony harmony = new Harmony(modGUID);
